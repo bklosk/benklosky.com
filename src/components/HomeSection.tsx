5502e20eaf12aeb18dd25d632e61150daff2e997
Ben Klosky <benklosky@uchicago.edu>
2025-07-04 16:58:15 -0500

    Implement code changes to enhance functionality and improve performance

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -28,21 +28,29 @@ export default function HomeSection() {
         </motion.p>
       </div>
 
-      <div className="absolute top-96 mt-32 left-1/2 transform -translate-x-1/2 md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 z-10">
+      <div className="absolute top-96 left-1/2 transform -translate-x-1/2 md:top-2/3 top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 z-10">
         <div className="relative">
           <Image
             src="/ben.png"
             alt="Ben Klosky"
             width={640}
             height={640}
-            className="w-72 h-72 md:w-96 md:h-96 object-contain rounded-full relative z-10"
+            className="w-72 h-72 md:w-96 md:h-96 object-contain rounded-full relative z-10 "
           />
           <Image
             src="/bike.png"
             alt="Ben Klosky"
             width={640}
             height={640}
-            className="w-72 h-72 md:w-96 md:h-96 object-contain rounded-full absolute top-1/3 left-3/4 transform -translate-x-1/2 -translate-y-1/2 rotate-[-50deg] z-0"
+            className="w-72 h-72 md:w-96 md:h-96 object-contain rounded-full absolute top-1/3 left-3/4 transform -translate-x-1/2 -translate-y-1/2 rotate-[-50deg] z-0 grayscale"
+          />
+          <Image
+            src="/piano.png"
+            alt="Ben Klosky"
+            width={640}
+            height={640}
+            className="w-72 h-72 md:w-96 md:h-96 object-contain rounded-full absolute top-1/4 left-1/6 transform -translate-x-1/2 -translate-y-1/2 rotate-[70deg] z-0 grayscale"
+            style={{ left: "calc(10%)" }}
           />
         </div>
       </div>
