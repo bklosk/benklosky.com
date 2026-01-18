aecc07ce3b561a1ba6d26a86f2a71de3df0ffe0a
Ben Klosky <benklosky@uchicago.edu>
2026-01-18 11:37:05 -0600

    Refactor Gallery component to improve accessibility and user experience; update styles and layout for better responsiveness

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,7 @@
+import posthog from 'posthog-js'
+
+posthog.init('phc_B2HuWHlorJ3GRZkvGtQ0zlrZEj1NM6tYYANwklR9cMT', {
+  api_host: 'https://us.i.posthog.com',
+  capture_pageview: true,
+  capture_pageleave: true,
+})
